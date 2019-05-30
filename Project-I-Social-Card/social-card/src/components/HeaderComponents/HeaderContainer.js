0c5243fd78321b019fe4bb84503d5118852b3081
leroyg <leroy.gardner@gmail.com>
2019-05-29 20:59:39 -0400

    Header and Body Container started

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
-import './Header.css';
+import "./Header.css";
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+
+function HeaderContainer() {
+    return (<div className="headingContainer">
+    <ImageThumbnail />
+    <div><HeaderTitle /> 
+    <HeaderContent /></div>
+    </div>
+    );
+}
+
+export default HeaderContainer;
