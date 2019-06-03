bb4ed05f4c3819e6a057890f032ed601d40b0eff
leroyg <leroy.gardner@gmail.com>
2019-06-03 09:40:25 -0400

    adjusting my buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -14,24 +14,28 @@ class App extends React.Component {
           <ActionButton className="three-text" text="clear" />
           <NumberButton bgColor="red-button" text="/" />
         </ButtonContainer>
+
         <ButtonContainer>
           <NumberButton bgColor="num-button" text="7" />
           <NumberButton bgColor="num-button" text="8" />
           <NumberButton bgColor="num-button" text="9" />
           <NumberButton bgColor="red-button" text="X" />
         </ButtonContainer>
+
         <ButtonContainer>
           <NumberButton bgColor="num-button" text="4" />
           <NumberButton bgColor="num-button" text="5" />
           <NumberButton bgColor="num-button" text="6" />
           <NumberButton bgColor="red-button" text="-" />
         </ButtonContainer>
+
         <ButtonContainer>
           <NumberButton bgColor="num-button" text="1" />
           <NumberButton bgColor="num-button" text="2" />
           <NumberButton bgColor="num-button" text="3" />
           <NumberButton bgColor="red-button" text="+" />
         </ButtonContainer>
+       
         <ButtonContainer>
           <ActionButton className="three-text" text="0" />
           <NumberButton bgColor="red-button" text="=" />
